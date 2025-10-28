47d1916552b189e60daf58e50b91dec8b96d7340
DESKTOP-DLOPF19\SSAFY <kimsiyeon5214@gmail.com>
2025-10-28 19:00:54 +0900

    Feat: 코딩테스트 연습

---
+++ PracCodingTest/src/백준/연습/Prac1000_A더하기B.java
@@ -0,0 +1,13 @@
+package 백준.연습;
+
+import java.util.Scanner;
+
+public class Prac1000_A더하기B {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(A + B);
+    }
+}
